94ea7afc02b8a2f416a2c3411c62039012b5aa84
Tommy Zieba <41349162+tzieba1@users.noreply.github.com>
2021-02-10 09:30:31 -0500

    Assignment 2 console work continued
    More work done to the assignment 2 console application to manage user input and prompt a user for a reverse polish expression calculator

--- AdvancedNETRepo/ReversePolishCalculator/ReversePolishCalculator/Program.cs
+++ AdvancedNETRepo/ReversePolishCalculator/ReversePolishCalculator/Program.cs
@@ -21,31 +21,40 @@ namespace ReversePolishCalculator
       bool running = true;                // Flag used to remain in loop displaying options.
       string userInput = string.Empty;    // Declaring the user input to be read from console.
 
-      while(running)
+      while (running)  // Loop until user enters 'exit'.
       {
         try
         {
           Console.Clear();
-          userInput = PromptForExpression();
-          displayResult(userInput);
+          userInput = PromptForExpression();  // Get user input expected to be a valid expression.
+          if (!displayResult(userInput))      // Calculate and display result, then check return - to continue or not.
+          {
+            running = false;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n\tEXITING REVERSE POLISH CALCULATOR... GOODBYE!\n");
+            Console.ForegroundColor = ConsoleColor.White;
+          }
         }
         catch (FormatException ex)
         {
           Console.Clear();
           Console.Beep();
           Console.ForegroundColor = ConsoleColor.Red;
-          Console.WriteLine("\n\t" + 
+          Console.WriteLine("\n\t" +
             "\n\n\tMUST INPUT VALID EXPRESSION IN REVERSE POLISH NOTATION!" +
             "\n\n\tPRESS ANY KEY TO CONTINUE...");
           Console.ForegroundColor = ConsoleColor.White;
           Console.ReadKey();
         }
-        catch(ArgumentOutOfRangeException ex)
+        catch (ArgumentOutOfRangeException ex)  // Thrown whenever provided an integer too large.
         {
           Console.Clear();
           Console.Beep();
           Console.WriteLine("\n\t" +
-            "");
+            "\n\n\tMUST INPUT VALID EXPRESSION WITH INTEGERS FROM -2,147,438,648 TO 2,147,483,647" +
+            "\n\n\tPRESS ANY KEY TO CONTINUE...");
+          Console.ReadKey();
         }
       }
     }
@@ -56,18 +65,21 @@ namespace ReversePolishCalculator
     /// <returns>Expected expression from user input into console.</returns>
     public static string PromptForExpression()
     {
-      Console.ForegroundColor = ConsoleColor.DarkCyan;
       Console.WriteLine(
-        " -----------------------------------------------------------------------------\n" +
-        " |  PLEASE INPUT A VALID MATHEMATICAL EXPRESSION IN REVERSE POLISH NOTATION  |\n" +
-        " -----------------------------------------------------------------------------\n" +
-        " |  NOTE: All whitespace around operators ignored and acceptable characters  |\n" +
-        " |        are in the set {+, -, *, /, ^, , 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}     |\n" +
-        " -----------------------------------------------------------------------------");
+        " ---------------------------------------------------------------------------\n" +
+        " |   PLEASE INPUT A VALID INTEGER EXPRESSION IN REVERSE POLISH NOTATION    |\n" +
+        " ---------------------------------------------------------------------------\n");
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine(
+        " |  NOTE: Whitespace ignored only around operators. Acceptable characters  |\n" +
+        " |        are in the set {+, -, *, /, ^, , 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}   |\n" +
+        " ---------------------------------------------------------------------------");
+      Console.ForegroundColor = ConsoleColor.White;
       Console.Write("\n  Example: ");
       Console.ForegroundColor = ConsoleColor.DarkGreen;
       Console.Write("15 7 1 1 + - / 3*2 1 1 ++ -\n\n");
       Console.ForegroundColor = ConsoleColor.White;
+      Console.Write("\n  Enter Expression: ");
       return Console.ReadLine();
     }
 
@@ -75,9 +87,34 @@ namespace ReversePolishCalculator
     /// Method used to display the result of a valid mathematical expression in reverse polish notaion.
     /// </summary>
     /// <param name="userInput"></param>
-    public static void displayResult(string userInput)
+    /// <returns>Boolean representing user's decision to continue or exit the program.</returns>
+    public static bool displayResult(string userInput)
     {
       Console.ReadKey();
+      //TODO: Calculate result of he expression tree.
+      double result = 0.0;
+
+      // Display result and prompt for user to continue calculating other expressions or exiting.
+      bool first = true;
+      while (true)
+      {
+        if (first) { Console.WriteLine($"Result: {result}"); }              // Only show result once.
+        Console.Write("Evaluate another reverse polish expression (y/n)?"); // Can repeatedly display.
+        string userDecision = Console.ReadKey().KeyChar.ToString();         // Detect user keystroke.
+
+        switch (userDecision.ToLower()) // Ensuring userDecision filtered to detect capital letters.
+        {
+          case "y":         // Return to prompt user for another expression (main prompt).
+            return true;
+          case "n":
+            return false;   // Return to exit the program.
+          default:
+            Console.Write("INVALID KEYSTROKE; TRY AGAIN.\n");
+            first = false;  // Prompt user for another decision when keystroke is invalid.
+            break;
+        }
+      }
+
     }
   }
 }
