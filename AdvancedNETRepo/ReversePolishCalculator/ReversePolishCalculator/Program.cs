2549a9adbfcdcf78d308e558ecd0cb8c9c750683
Tommy Zieba <41349162+tzieba1@users.noreply.github.com>
2021-02-13 21:36:22 -0500

    Advanced .NET assignment 2 work
    Working on the builder/visitor class with additional properties and methods for iteratively updating the root node and usingthe overridden ExpressionVisitor methods

--- AdvancedNETRepo/ReversePolishCalculator/ReversePolishCalculator/Program.cs
+++ AdvancedNETRepo/ReversePolishCalculator/ReversePolishCalculator/Program.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace ReversePolishCalculator
@@ -15,28 +16,35 @@ namespace ReversePolishCalculator
   {
     public static void Main(string[] args)
     {
-      bool running = true;                // Flag used to remain in loop displaying options.
-      string userInput = string.Empty;    // Declaring the user input to be read from console.
+      Console.WindowWidth = 79;
+      bool running = true;      // Flag used to remain in loop displaying options.
+      string userInput;         // Declaring the user input to be read from console.
 
-      while (running)  // Loop until user enters 'exit'.
+      // Loop until user enters 'exit' in expression prompt or chooses option 'n' to finish evaluating expressions.
+      while (running)
       {
         try
         {
           Console.Clear();
-          userInput = PromptForExpression();  // Get user input expected to be a valid expression.
-          if (!displayResult(userInput))      // Calculate and display result, then check return - to continue or not.
+          userInput = PromptForExpression();  // Get user input.
+
+          // Stop running when user enters 'exit'.
+          if (userInput.ToLower().Equals("exit"))   
+            running = false;
+          // Calculate and display result and check return - to continue running or not.
+          else if (!displayResult(userInput))
           {
             running = false;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("\n\tEXITING REVERSE POLISH CALCULATOR... GOODBYE!\n");
-            Console.ForegroundColor = ConsoleColor.White;
           }
         }
         catch (FormatException ex)
         {
           Console.Clear();
           Console.Beep();
+          Console.WriteLine($"\n\tError: {ex.Message}");
           Console.ForegroundColor = ConsoleColor.Red;
           Console.WriteLine("\n\t" +
             "\n\n\tMUST INPUT VALID EXPRESSION IN REVERSE POLISH NOTATION!" +
@@ -48,6 +56,7 @@ namespace ReversePolishCalculator
         {
           Console.Clear();
           Console.Beep();
+          Console.WriteLine($"\n\tError: {ex.Message}");
           Console.WriteLine("\n\t" +
             "\n\n\tMUST INPUT VALID EXPRESSION WITH INTEGERS FROM -2,147,438,648 TO 2,147,483,647" +
             "\n\n\tPRESS ANY KEY TO CONTINUE...");
@@ -63,18 +72,16 @@ namespace ReversePolishCalculator
     public static string PromptForExpression()
     {
       Console.WriteLine(
-        " ---------------------------------------------------------------------------\n" +
-        " |   PLEASE INPUT A VALID INTEGER EXPRESSION IN REVERSE POLISH NOTATION    |\n" +
-        " ---------------------------------------------------------------------------\n");
+        "\n  ---------------------------------------------------------------------------\n" +
+        "  |   PLEASE INPUT A VALID INTEGER EXPRESSION IN REVERSE POLISH NOTATION    |\n" +
+        "  ---------------------------------------------------------------------------\n");
       Console.ForegroundColor = ConsoleColor.Red;
       Console.WriteLine(
-        " |  NOTE: Whitespace ignored only around operators. Acceptable characters  |\n" +
-        " |        are in the set {+, -, *, /, ^, , 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}   |\n" +
-        " ---------------------------------------------------------------------------");
-      Console.ForegroundColor = ConsoleColor.White;
-      Console.Write("\n  Example: ");
-      Console.ForegroundColor = ConsoleColor.DarkGreen;
-      Console.Write("15 7 1 1 + - / 3*2 1 1 ++ -\n\n");
+        "  |  NOTE: Whitespace ignored only around operators. Acceptable characters  |\n" +
+        "  |        are in the set {+, -, *, /, ^, , 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}   |\n" +
+        "  ---------------------------------------------------------------------------");
+      Console.ForegroundColor = ConsoleColor.DarkCyan;
+      Console.Write("\n  Example:\t15 7 1 1 + - / 3*2 1 1 ++ -\n");
       Console.ForegroundColor = ConsoleColor.White;
       Console.Write("\n  Enter Expression: ");
       return Console.ReadLine();
@@ -87,19 +94,20 @@ namespace ReversePolishCalculator
     /// <returns>Boolean representing user's decision to continue or exit the program.</returns>
     public static bool displayResult(string userInput)
     {
-      Console.ReadKey();
-      //TODO: Validate the expression input from the user.
-      Expression validExpression = validateExpression(userInput);
-      //TODO: Calculate result of he expression tree.
-      double result = calculateExpression(userInput);
+      validateCharacters(userInput);  // Validate characters in expression from user input.
+      ReversePolishExpressionBuilder expressionBuilder = new ReversePolishExpressionBuilder(userInput);
+
+      //TODO: Calculate result of expression tree using current RootExpression from expression builder.
+      double result = calculateExpression(expressionBuilder.RootExpression);
 
       // Display result and prompt for user to continue calculating other expressions or exiting.
       bool first = true;
       while (true)
       {
-        if (first) { Console.WriteLine($"  Result: {result}"); }              // Only show result once.
-        Console.Write("  Evaluate another reverse polish expression? (y/n)"); // Can repeatedly display.
-        string userDecision = Console.ReadKey().KeyChar.ToString();         // Detect user keystroke.
+        if (first) { Console.WriteLine($"  Result: {result}"); }                // Only show result once.
+        Console.Write("\n  Evaluate another reverse polish expression? (y/n)"); // Can repeatedly display.
+        Console.CursorLeft = 53;                                                // Adjust cursor position.
+        string userDecision = Console.ReadKey().KeyChar.ToString();             // Detect user keystroke.
 
         switch (userDecision.ToLower()) // Ensuring userDecision filtered to detect capital letters.
         {
@@ -108,7 +116,9 @@ namespace ReversePolishCalculator
           case "n":
             return false;   // Return to exit the program.
           default:
-            Console.Write("  INVALID KEYSTROKE; TRY AGAIN.\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n  INVALID KEYSTROKE; TRY AGAIN.\n");
+            Console.ForegroundColor = ConsoleColor.White;
             first = false;  // Prompt user for another decision when keystroke is invalid.
             break;
         }
@@ -117,102 +127,40 @@ namespace ReversePolishCalculator
     }
 
     /// <summary>
-    /// Method that validates user input to be a proper expression in reverse polish notation and
-    /// has integers within acceptable limits for the data type int.
+    /// Method that validates user input as an expression in reverse polish notation and has integers within acceptable limits.
     /// </summary>
     /// <param name="userInput"></param>
     /// <returns></returns>
-    private static Expression validateExpression(string userInput)
+    private static void validateCharacters(string userInput)
     {
-      string validPattern = @"[\s\+\-\*\/\^0123456789]";      // Valid operators and digits in positive character group.
+      string validPattern = @"[\s\+\-\*\/\^0123456789]";      // Valid operators, numbers, and whitespace in positive character group.
       string invalidPattern = @"[^\s\+\-\*\/\^0123456789]";   // Negation of valid character group (all other chars).
 
-      if (Regex.IsMatch(userInput, validPattern) && !Regex.IsMatch(userInput, invalidPattern))  // Validate characters.
+      // Validate characters (whitespace, numbers, operators).
+      if (Regex.IsMatch(userInput, validPattern) && !Regex.IsMatch(userInput, invalidPattern))  
       {
-        if (Regex.IsMatch(userInput[0].ToString(), @"[\d]"))  // Validate first character is integer.
-        {
-          Expression builtExpression = buildExpressionTree(userInput);
-        }
-        else
+        // Validate first non-whitespace character of user input is a number.
+        if (!Regex.IsMatch(userInput, @"^\s*[\d]"))
           throw new FormatException("Expression must start with number.");
+
+        // Validate last non-whitespace character of user input is an operator.
+        if (!Regex.IsMatch(userInput, @"[\+\-\*\/\^]\s*$"))
+          throw new FormatException("Expression must end with an operator.");
       }
       else
         throw new FormatException("Invalid characters present.");
-
-      return null;
     }
 
-    /// <summary>
-    /// Builds an expression tree from validated user input and returns it.
-    /// </summary>
-    /// <param name="userInput"></param>
-    /// <returns>Expression representing userInput.</returns>
-    private static Expression buildExpressionTree(string userInput)
-    {
-      Stack<ConstantExpression> partialExpressionTree = new Stack<ConstantExpression>();
-
-      //TODO: Stack constant expressions until an operator is reached, then unstack constants and build expression 
-      //      (throw excpetion if another number reached before all constants unstacked or invalid character detected).
-      foreach (char character in userInput)
-      {
-        if (!character.Equals(' ') && !Regex.IsMatch(character.ToString(), @"[\+\-\*\/\^]")) //Check for non-whitespace characters to stack constants.
-          partialExpressionTree.Push(Expression.Constant(int.Parse(character.ToString()))); // Throws exception if non-int.
-        else if (!Regex.IsMatch(character.ToString(), @"[\+\-\*\/\^]"))   
-        {
-          //TODO: Build partial expression by unstacking constants and throw exception if stack is not empty upon reaching next constant.
-          Expression<Func<double, double, double>> partialBinaryExpression;
-          switch (character)
-          {
-            case '+':
-              partialBinaryExpression = (c1, c2) => c1 + c2;
-              break;
-            case '-':
-              partialBinaryExpression = (c1, c2) => c1 - c2;
-              break;
-            case '*':
-              partialBinaryExpression = (c1, c2) => c1 * c2;
-              break;
-            case '/':
-              partialBinaryExpression = (c1, c2) => c1 / c2;
-              break;
-            case '^':
-              partialBinaryExpression = (c1, c2) => Math.Pow(c1, c2);
-              break;
-            default:
-              break;
-          }
-        }
-        else  // Throw exception for an invalid character detected (not needed based on previous validation?)
-        {
-          throw new ArgumentException($"'{character}' is not a valid character for any supported reverse polish expression.");
-        }
-      }
-
-      
-      //TODO: Pass each valid partial expression to the expression visitor
-      //int charIndex = 0;
-      //while (!Regex.IsMatch(userInput[charIndex].ToString(), @"[\+\-\*\/\^]"))
-      //{
-      //  //TODO: Check for non-whitespace characters to stack constants.
-      //  if(!userInput[charIndex].Equals(' '))
-      //  {
-      //    partialExpressionTree.Push(userInput[charIndex]);
-      //  }
-
-      //  charIndex++;  // Increment index of expression string characters.
-      //}
-
-      return null;
-    }
+   
 
     /// <summary>
     /// Method that accepts an expression
     /// </summary>
     /// <param name="userInput"></param>
     /// <returns></returns>
-    private static double calculateExpression(string userInput)
+    private static double calculateExpression(Expression expressionTree)
     {
-      throw new NotImplementedException();
+      return 0.0;
     }
   }
 }
---
+++ AdvancedNETRepo/ReversePolishCalculator/ReversePolishCalculator/ReversePolishExpressionBuilder.cs
@@ -0,0 +1,192 @@
+﻿/// Author:           Tommy Zieba
+/// Student Number:   000797152
+/// 
+/// I, Tommy Zieba, 000797152 certify that this material is my original work. 
+/// No other person's work has been used without due acknowledgement.
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReversePolishCalculator
+{
+  /// <summary>
+  /// Expression visitor used to build reverse polish expressions by accepting string input from console.
+  /// </summary>
+  public class ReversePolishExpressionBuilder : ExpressionVisitor
+  {
+    /// <summary>
+    /// Prooperty representing the root expression for this reverse polish expression tree visitor.
+    /// Note that this must be reset after partial expressions are parsed and appended to the 
+    /// expression tree being built.
+    /// </summary>
+    public Expression RootExpression { get; set; }
+
+    /// <summary>
+    /// Property that gets ExpressionString, or custom sets it by also building an expression, and 
+    /// setting it as this class' RootExpression to be evaluated.
+    /// </summary>
+    public string ExpressionString
+    {
+      get { return this.ExpressionString; }
+      set
+      {
+        this.ExpressionString = value;
+        BuildExpressionTree(value);
+      }
+    }
+
+    /// <summary>
+    /// Operators stacked for building expressions.
+    /// </summary>
+    private Stack<char> operatorStack = new Stack<char>();
+    
+    /// <summary>
+    /// Constants to be formulated into an expression.
+    /// </summary>
+    private Stack<ConstantExpression> constantExpressionStack = new Stack<ConstantExpression>();
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public ReversePolishExpressionBuilder() { }
+
+    /// <summary>
+    /// Constructor that accepts a root node (constant expression) to begin building a reverse
+    /// polish expression. Root assigned to partialExpressionStack until first operator reached.
+    /// </summary>
+    /// <param name="root"></param>
+    public ReversePolishExpressionBuilder(string expressionString) { this.ExpressionString = expressionString; }
+
+    /// <summary>
+    /// Builds an expression tree from validated user input and returns it.
+    /// </summary>
+    /// <param name="userInput"></param>
+    /// <returns>Expression representing userInput.</returns>
+    private void BuildExpressionTree(string expressionString)
+    {
+      Regex whiteSpaceRegex = new Regex(@"\s+", RegexOptions.Compiled); // Pattern used to split and extract numbers and/or operators.
+      string[] sanitizedUserInput = whiteSpaceRegex.Split(expressionString);   // String array containing numbers and/or operators only.
+
+      // Handle character sequences without whitespace and having both numbers and operators - where an expression is needed.
+      for (int i = 0; i < sanitizedUserInput.Length; i++)
+      {
+        string nonWhiteSpaceSequence = sanitizedUserInput[i];
+        // Check for operators present in a non-whitespace sequence, then build expression using custom reverse polish visitor.
+        if (!Regex.IsMatch(nonWhiteSpaceSequence, @"[\+\-\*\/\^]"))
+          constantExpressionStack.Push(Expression.Constant(int.Parse(nonWhiteSpaceSequence)));
+        
+        //When no operators present, stack the constant expression.
+        else
+        {
+          Regex splitOperatorsRegex = new Regex(@"[0123456789]", RegexOptions.Compiled);  // Pattern used to split and extract operators.
+          Regex splitNumbersRegex = new Regex(@"[\+\-\*\/\^]", RegexOptions.Compiled);    // Pattern used to split and extract numbers.
+
+          string[] numbers = splitNumbersRegex.Split(nonWhiteSpaceSequence);          // Split sequence to extract numbers.
+          //Array.ForEach(numbers, Console.WriteLine);
+          string[] operators = splitOperatorsRegex.Split(nonWhiteSpaceSequence);      // Split sequence to extract operators.   
+          foreach (char operation in operators[0]) { operatorStack.Push(operation); } // Stack operators.
+
+
+          // Set reverse polish visitor root Expression node initially.
+          if (i == 0)
+          {
+            this.RootExpression = Expression.Constant(int.Parse(numbers[0]));
+          }
+          // Reset root Expression node while an operator and constant expression remains to join to it.
+          else
+          {
+            do  // Check that there is an operator remaining to join constant expression to root node and join expression.
+            {
+              if (this.constantExpressionStack.Count > 0 && this.operatorStack.Count == 0)    // Not enough operators.
+                throw new FormatException("Operators missing to join LHS constant expression to root expression.");
+              if (this.constantExpressionStack.Count == 0 && this.operatorStack.Count > 1)    // Not enough constants.
+                throw new FormatException("Constants missing to join LHS constant expression to root expression.");
+
+              this.RootExpression = this.Visit(this.RootExpression);  // Iteratively modifies this builder's RootExpression.
+            } while (operatorStack.Count != 0 || constantExpressionStack.Count != 0);
+          }
+
+          // Finally, check case for 2 numbers attached to operator(s) to add second number to empty constantStack 
+          //    e.g. 2 3 9*+9 7-- 2 1 +*3 4-- contains 9*+9 and 7-- and +*3
+          if (numbers.Length == 2)
+            this.constantExpressionStack.Push(Expression.Constant(int.Parse(numbers[1])));
+        }     
+      }
+    }
+
+    /// <summary>
+    /// Method used to join constant expressions with existing rootExpression for this visitor/builder.
+    /// </summary>
+    /// <param name="constantExpression"></param>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    private Expression JoinExpression(ConstantExpression constantExpression, char operation)
+    {
+     
+
+      return null;
+    }
+
+    /// <summary>
+    /// Method passes control of an expression node to correct method for visiting binary or constant
+    /// expression nodes according to supported expression types required for operations 
+    ///  +, -, *, /, and ^.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public override Expression Visit(Expression node)
+    {
+      // Build binary expression by visiting root to create a new root expression based 
+      // on constantExpression and operation passed in.
+      char operation = this.operatorStack.Pop();
+      switch (operation)
+      {
+        case '+':
+          return Expression.MakeBinary(ExpressionType.Add, this.RootExpression, constantExpressionStack.Pop());
+        case '-':
+          return Expression.MakeBinary(ExpressionType.Subtract, this.RootExpression, constantExpressionStack.Pop());
+        case '*':
+          return Expression.MakeBinary(ExpressionType.Multiply, this.RootExpression, constantExpressionStack.Pop());
+        case '/':
+          return Expression.MakeBinary(ExpressionType.Divide, this.RootExpression, constantExpressionStack.Pop());
+        case '^':
+          return Expression.MakeBinary(ExpressionType.Power, this.RootExpression, constantExpressionStack.Pop());
+        default:
+          throw new FormatException("Never reached.");
+      }
+    }
+
+    /// <summary>
+    /// Visiting a binary expression node returns it in case it has a valid ExpressionType.
+    /// Valid binary expression types are for operators +, -, *, /, and ^.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+      switch (node.NodeType)
+      {
+        case ExpressionType.Add:
+        case ExpressionType.Subtract:
+        case ExpressionType.Multiply:
+        case ExpressionType.Divide:
+        case ExpressionType.Power:
+          var right = this.Visit(node.Right);
+          var left = this.Visit(node.Left);
+          return Expression.MakeBinary(ExpressionType.Multiply, left, right);
+        default:
+          return base.VisitBinary(node);
+      }
+    }
+
+    /// <summary>
+    /// Visiting a constant expression returns it.
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    protected override Expression VisitConstant(ConstantExpression node) { return this.Visit(node); }
+  }
+}
